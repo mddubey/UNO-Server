e7e7b9df0c98bbeae94ede02a6f5156492fd14b7
tanbirka <kaurTanbir>
2014-04-22 12:15:59 +0530

    game master screen changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var SampleApp = function () {
     self.setupVariables = function () {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port = process.env.OPENSHIFT_NODEJS_PORT || 5001;
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8088;
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
